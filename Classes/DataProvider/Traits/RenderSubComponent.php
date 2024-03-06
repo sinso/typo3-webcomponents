31b5ba0a5b4699f3e0f07a5d56cd7ab18ef127e6
Sebastian Michaelsen <sebastian@michaelsen.io>
2024-03-06 10:12:21 +0100

    fix: adapt RenderSubComponent to new interface

--- Classes/DataProvider/Traits/RenderSubComponent.php
+++ Classes/DataProvider/Traits/RenderSubComponent.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Sinso\Webcomponents\DataProvider\Traits;
 
 use Sinso\Webcomponents\DataProvider\DataProviderInterface;
+use Sinso\Webcomponents\Dto\WebcomponentRenderingData;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 trait RenderSubComponent
@@ -12,21 +13,24 @@ trait RenderSubComponent
     use ContentObjectRendererTrait;
     use RenderComponent;
 
-    protected function renderSubComponent(string $dataProviderClassName, $inputData): ?string
+    protected function renderSubComponent(string $dataProviderClassName, $inputData = []): ?string
     {
         $dataProvider = GeneralUtility::makeInstance($dataProviderClassName);
         if (!$dataProvider instanceof DataProviderInterface) {
             throw new \RuntimeException('DataProvider must implement DataProviderInterface');
         }
         $dataProvider->setContentObjectRenderer($this->contentObjectRenderer);
-        $tagName = $dataProvider->getTagName();
-        $properties = $dataProvider->getProperties($inputData);
-        $content = $dataProvider->getContent($inputData);
+        $webComponentRenderingData = GeneralUtility::makeInstance(WebcomponentRenderingData::class);
+        $webComponentRenderingData = $dataProvider->provide($inputData, $webComponentRenderingData);
 
-        if ($tagName === null || $properties === null) {
+        if (!$webComponentRenderingData->isRenderable()) {
             return null;
         }
 
-        return $this->renderComponent($tagName, $content, $properties);
+        return $this->renderComponent(
+            $webComponentRenderingData->getTagName(),
+            $webComponentRenderingData->getContent(),
+            $webComponentRenderingData->getProperties()
+        );
     }
 }
