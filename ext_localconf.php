56364847843dc63369cebb18de3d05a19e2a3bc7
Sebastian Michaelsen <sebastian@michaelsen.io>
2024-07-28 07:58:54 +0200

    fix: Safe array access in localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,6 @@
 
 defined('TYPO3') or die();
 
-$GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'] = array_merge($GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'], [
+$GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'] = array_merge($GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'] ?? [], [
     'WEBCOMPONENT' => \Sinso\Webcomponents\ContentObject\WebcomponentContentObject::class,
 ]);
