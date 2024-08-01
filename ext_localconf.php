616c2ad20c3c09e9ef586d8b564159a4c68f522d
Sebastian Michaelsen <sebastian@michaelsen.io>
2024-08-01 10:37:06 +0200

    chore!: mark as compatible with TYPO3 v13, drop v11

--- ext_localconf.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('TYPO3') or die();
-
-$GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'] = array_merge($GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'] ?? [], [
-    'WEBCOMPONENT' => \Sinso\Webcomponents\ContentObject\WebcomponentContentObject::class,
-]);
