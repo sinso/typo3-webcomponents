11da8c4e62e3ddaa27c4c76119f6e212ae5dbdbb
Sebastian Michaelsen <sebastian@michaelsen.io>
2025-04-15 13:42:06 +0200

    wip

--- Classes/ViewHelpers/RenderViewHelper.php
+++ Classes/ViewHelpers/RenderViewHelper.php
@@ -70,4 +70,34 @@ class RenderViewHelper extends AbstractTagBasedViewHelper
         $logger = $logManager->getLogger(__CLASS__);
         $logger->warning('Component evaluation failed', ['exception' => $e]);
     }
+
+    protected function registerUniversalTagAttributes(): void
+    {
+        parent::registerUniversalTagAttributes();
+
+        // the list from fluid is far from complete, so we add more possible attributes
+        $this->registerTagAttribute('autocapitalize', 'string', 'Specifies how the text input should be capitalized. Allowed values: "none", "sentences", "words", "characters"');
+        $this->registerTagAttribute('autocorrect', 'string', 'Specifies whether the browser should attempt to automatically correct the text input. Allowed values: "on", "off"');
+        $this->registerTagAttribute('autofocus', 'boolean', 'Specifies whether the element should automatically get focus when the page loads');
+        $this->registerTagAttribute('contenteditable', 'boolean', 'Specifies whether the element is editable or not');
+        $this->registerTagAttribute('draggable', 'string', 'Specifies whether the element is draggable or not. Allowed values: "true", "false"');
+        $this->registerTagAttribute('enterkeyhint', 'string', 'Specifies the hint for the Enter key. Allowed values: "enter", "done", "go", "next", "search", "send"');
+        $this->registerTagAttribute('exportparts', 'string', 'Allows you to select and style elements existing in nested shadow trees, by exporting their part names.');
+        $this->registerTagAttribute('hidden', 'string', 'Allows you to hide an element. The element will not be displayed, but it will still be present in the DOM. Allowed values: "hidden", "until-found"');
+        $this->registerTagAttribute('inert', 'boolean', 'Specifies whether the element and its descendants are inert. Inert elements are not focusable and do not respond to events.');
+        $this->registerTagAttribute('inputmode', 'string', 'Allowed values: "none", "text", "tel", "url", "email", "numeric", "decimal", "search"');
+        $this->registerTagAttribute('itemid', 'string', 'Microdata itemid');
+        $this->registerTagAttribute('itemprop', 'string', 'Microdata itemprop');
+        $this->registerTagAttribute('itemref', 'string', 'Microdata itemref');
+        $this->registerTagAttribute('itemscope', 'string', 'Microdata itemscope');
+        $this->registerTagAttribute('itemtype', 'string', 'Microdata itemtype');
+        $this->registerTagAttribute('popover', 'string', 'Allowed values: "auto", "hint", "manual"');
+        $this->registerTagAttribute('slot', 'string', 'Placement of the element in the shadow DOM of the containing element');
+        $this->registerTagAttribute('spellcheck', 'string', 'Specifies whether the element should be checked for spelling errors. Allowed values: "true", "false"');
+        $this->registerTagAttribute('translate', 'string', 'Specifies whether the element should be translated or not. Allowed values: "yes", "no"');
+        $this->registerTagAttribute('writingsuggestions', 'string', 'Specifies whether the element should show writing suggestions. Allowed values: "true", "false"');
+
+        // and these attributes are technically not universal, but very common and possibly useful for web components
+        $this->registerTagAttribute('name', 'string', 'Name of the element');
+    }
 }
