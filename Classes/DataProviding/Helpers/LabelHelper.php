b50f1f5057f3b1a5468507854696337d5907ea55
Sebastian Michaelsen <sebastian@michaelsen.io>
2025-07-23 06:21:26 +0200

    fix: make LabelHelper work without initialised TypoScript

--- Classes/DataProviding/Helpers/LabelHelper.php
+++ Classes/DataProviding/Helpers/LabelHelper.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Sinso\Webcomponents\DataProviding\Helpers;
 
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\TypoScript\FrontendTypoScript;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 
 class LabelHelper
@@ -11,8 +13,23 @@ class LabelHelper
     /**
      * @param (string|int)[] $arguments
      */
-    public function translate(string $extensionName, string $id, array $arguments = []): ?string
+    public function translate(?string $extensionName, string $id, array $arguments = []): ?string
     {
+        if (!$this->isFrontendTypoScriptAvailable() && !empty($extensionName)) {
+            // If Frontend TypoScript is not available, extensionName must not be used - because LocalizationUtility will try to load overwriting labels from TypoScript
+            $id = 'LLL:EXT:' . $extensionName . '/Resources/Private/Language/locallang.xlf:' . $id;
+            $extensionName = null;
+        }
         return LocalizationUtility::translate($id, $extensionName, $arguments) ?: null;
+    }
+
+    private function isFrontendTypoScriptAvailable(): bool
+    {
+        return $this->getRequest()?->getAttribute('frontend.typoscript') instanceof FrontendTypoScript;
+    }
+
+    private function getRequest(): ?ServerRequestInterface
+    {
+        return $GLOBALS['TYPO3_REQUEST'] ?? null;
     }
 }
